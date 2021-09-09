9c0095be1ab6c766419ceef28181a4a5d69dd828
frontier <2634575822qq.com>
2021-09-09 16:10:34 +0800

    add helloworld

---
+++ lianxi/git-worlk/src/com/company/helloworld.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * @program: git-worlk
+ * @description:
+ * @author: Q3
+ * @create: 2021-09-09 16:
+ */
+public class helloworld {
+    public static void main(String[] args){
+        System.out.println("你好");
+    }
+}
