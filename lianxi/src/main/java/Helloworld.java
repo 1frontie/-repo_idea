a6d1decf0f73e5f5288b559fc67bf04ecc665775
frontier <2634575822qq.com>
2021-09-09 09:23:38 +0800

    first commit

---
+++ lianxi/src/main/java/Helloworld.java
@@ -0,0 +1,11 @@
+/**
+ * @program: hello_maven
+ * @description:
+ * @author: Q3
+ * @create: 2021-09-07 12:
+ */
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
